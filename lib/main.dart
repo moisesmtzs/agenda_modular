6d8ea112a325b775fc306e6fe417d4a47915bfb7
moisesmtzs <moises.martinez6605@alumnos.udg.mx>
2022-06-06 19:36:06 -0500

    Definicion de gamas de colores para la aplicacion

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,49 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+        debugShowCheckedModeBanner: false,
+        title: 'Agenda App',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Agenda App - Modular'),
           ),
+          body: Center(
+            child: Column(
+              children: const [
+                Icon(Icons.access_time_outlined),
+                SizedBox(height: 35),
+                Text("Hola guapo")
+
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon( Icons.add )
+          )
         ),
-      ),
-    );
+        theme: ThemeData.light().copyWith(
+          // colorScheme: ColorScheme(secondary: Colors.deepPurple[200]),
+            scaffoldBackgroundColor: Colors.grey[300],
+            textSelectionTheme: TextSelectionThemeData(
+                selectionColor: Colors.grey[350],
+                selectionHandleColor: Colors.deepPurple[400],
+                cursorColor: Colors.deepPurple),
+            appBarTheme: AppBarTheme(
+              elevation: 0,
+              color: Colors.indigo[300],
+            ),
+            floatingActionButtonTheme: FloatingActionButtonThemeData(
+              backgroundColor: Colors.orange[300], elevation: 0)
+          )
+        );
   }
 }
