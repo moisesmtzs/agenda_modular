2654248e1ee5a2501f9aeeb781f4f8c33b8a0f52
moisesmtzs <moises.martinez6605@alumnos.udg.mx>
2022-06-07 21:27:33 -0500

    Login y registro

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:get/get.dart';
+
+import 'package:agenda_app/src/screens/screens.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
@@ -10,41 +14,32 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Agenda App',
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Agenda App - Modular'),
-          ),
-          body: Center(
-            child: Column(
-              children: const [
-                Icon(Icons.access_time_outlined),
-                SizedBox(height: 35),
-                Text("Hola guapo")
-
-              ],
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Icon( Icons.add )
-          )
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Agenda App',
+      initialRoute: '/',
+      getPages: [
+        GetPage( name: '/', page: () => LoginPage() ),
+        GetPage( name: '/register', page: () => RegisterPage() )
+      ],
+      navigatorKey: Get.key,
+      theme: ThemeData.light().copyWith(
+        primaryColor: Colors.indigo[300],
+        colorScheme: ColorScheme(
+          // backgroundColor: Colors.orange[300], elevation: 0
+          primary: Colors.indigo.shade300,
+          secondary: Colors.indigoAccent, 
+          background: Colors.grey.shade300, 
+          brightness: Brightness.light, 
+          error: Colors.grey.shade300, 
+          surface: Colors.grey.shade300,
+          onBackground: Colors.grey.shade300, 
+          onError: Colors.grey.shade300, 
+          onPrimary: Colors.grey.shade300, 
+          onSecondary: Colors.grey.shade300, 
+          onSurface: Colors.grey.shade300, 
         ),
-        theme: ThemeData.light().copyWith(
-          // colorScheme: ColorScheme(secondary: Colors.deepPurple[200]),
-            scaffoldBackgroundColor: Colors.grey[300],
-            textSelectionTheme: TextSelectionThemeData(
-                selectionColor: Colors.grey[350],
-                selectionHandleColor: Colors.deepPurple[400],
-                cursorColor: Colors.deepPurple),
-            appBarTheme: AppBarTheme(
-              elevation: 0,
-              color: Colors.indigo[300],
-            ),
-            floatingActionButtonTheme: FloatingActionButtonThemeData(
-              backgroundColor: Colors.orange[300], elevation: 0)
-          )
-        );
+      )
+    );
   }
 }
